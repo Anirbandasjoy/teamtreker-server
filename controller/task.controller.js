56180df369d4e16a105073d9b1c59212065b5f49
Anirbandasjoy <joy600508@gmail.com>
2024-04-26 02:36:18 +0600

    get task using assignEmployeeId

--- controller/task.controller.js
+++ controller/task.controller.js
@@ -33,7 +33,6 @@ const createNewTask = async (req, res) => {
         return res.status(500).json({ message: "Internal server error" });
       }
 
-      console.log("Task created successfully:", result);
       res
         .status(201)
         .json({ message: "Task created successfully", task: req.body });
@@ -45,6 +44,86 @@ const createNewTask = async (req, res) => {
   }
 };
 
+const findAlltask = (req, res) => {
+  try {
+    // Create SQL query
+    const sql = `SELECT * FROM tasks`;
+    // Execute query
+    pool.query(sql, (err, results) => {
+      if (err) {
+        console.error("Error executing query:", err);
+        return res.status(500).json({ message: "Internal server error" });
+      }
+
+      console.log("Tasks fetched successfully:", results);
+      res.status(200).json(results);
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: "Internal server error", error: error.message });
+  }
+};
+
+const findSingleTask = (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const sql = `SELECT * FROM tasks WHERE id = ?`;
+
+    pool.query(sql, [id], (err, results) => {
+      if (err) {
+        console.error("Error executing query:", err);
+        return res.status(500).json({ message: "Internal server error" });
+      }
+
+      // Check if task was found
+      if (results.length === 0) {
+        return res.status(404).json({ message: "Task not found" });
+      }
+
+      console.log("Task fetched successfully:", results[0]);
+      res.status(200).json(results[0]);
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+const findTasksByEmployeeId = (req, res) => {
+  try {
+    const id = req.params.id;
+    console.log({ id });
+    const sql = `SELECT * FROM tasks WHERE assigned_employee_id = ?`;
+
+    pool.query(sql, [id], (err, results) => {
+      if (err) {
+        console.error("Error executing query:", err);
+        return res.status(500).json({ message: "Internal server error" });
+      }
+
+      // Check if tasks were found
+      if (results.length === 0) {
+        return res
+          .status(404)
+          .json({ message: "No tasks found for this employee" });
+      }
+
+      console.log("Tasks fetched successfully for employee ID:", id);
+      res.status(200).json(results);
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
 module.exports = {
   createNewTask,
+  findAlltask,
+  findSingleTask,
+  findTasksByEmployeeId,
 };
--- routes/task.router.js
+++ routes/task.router.js
@@ -1,7 +1,15 @@
-const { createNewTask } = require("../controller/task.controller");
+const {
+  createNewTask,
+  findAlltask,
+  findSingleTask,
+  findTasksByEmployeeId,
+} = require("../controller/task.controller");
 
 const taskRouter = require("express").Router();
 
 taskRouter.post("/create", createNewTask);
+taskRouter.get("/find", findAlltask);
+taskRouter.get("/find-single-task/:id", findSingleTask);
+taskRouter.get("/findBy-employeeId/:id", findTasksByEmployeeId);
 
 module.exports = taskRouter;
